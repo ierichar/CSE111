a298cc4f8d9ff6a225923881e578bb3bc9b77661
Ian Richardson <ierichar@ucsc.edu>
2021-10-09 10:49:53 -0700

    Jackson's pseudocode

--- asg1-dc-bigint/code/bigint.cpp
+++ asg1-dc-bigint/code/bigint.cpp
@@ -23,61 +23,64 @@ bigint::bigint (const string& that) {
 }
 
 bigint bigint::operator+ () const {
+   return *this;
+}
+
+bigint bigint::operator- () const {
+   return {uvalue, not is_negative};
+}
+
+bigint bigint::operator+ (const bigint& that) const {
+   ubigint result {uvalue + that.uvalue};
+   //are strings same size?
+      //create a flag, isBLarger? yes, run first loop! no? run second loop.
+
+   //is B string larger than A string?
+      //same code as below but flip B and A when calling value!
+
    //is A string larger than B string?
       //yes!
          //are the signs the same?
             //yes!
                //are they positive?
                   //yes!
-                     //Run addition (A+B)
+                     //Run addition (A+B) (A+B, keep +)
                   //no!
-                     //Run addition flip sign(-A+-B)
-   return *this;
-}
-
-bigint bigint::operator- () const {
-   // WORK IN PROGRESS
-   //first step, check size of A and B
-
-   //is A larger than B?
-      //it's gonna  stay the sign of the larger #
-   //is B larger than A?
-      //it's gonna stay the sign of the larger #
-   //else, They are the same size
-
-   //Are they the same sign?
-      //yes?
-         //is the larger string first?
-            //yes?
-               //stays that sign
-            //no?
-               //flip signs
-      //no? 
-         //is the larger string first?
-            //yes?
-               //stays that sign
-            //no?
-               //flip sign
-
-
-
-   //subbing two pos #'s that leads to a positive
-
-   // subbing two pos #'s that lead to a negative
-
-   //subbing two neg #'s that lead to a positive
-
-   //subbing two neg #'s that lead to a negative
-   return {uvalue, not is_negative};
-}
-
-bigint bigint::operator+ (const bigint& that) const {
-   ubigint result {uvalue + that.uvalue};
+                     //Run addition flip sign(-A+-B) (A+B, keep -)
+            //no!
+               //is A negative?
+                  //yes!
+                     //run subtraction (-a+b) A is larger, A-B, keep -
+                  //no!
+                     //run subtraction (a+-b)  A is larger, A-B
+      //no!
    return result;
 }
 
 bigint bigint::operator- (const bigint& that) const {
    ubigint result {uvalue - that.uvalue};
+   // SUBTRACTION
+   //are strings same size?
+         //create a flag, isBLarger? yes, run first loop! no? run second loop.
+
+      //is B string larger than A string?
+         //same code as below but flip B and A when calling value!
+
+   //is A string larger than B string?
+      //Yes!
+         //are the signs the same?
+            //yes!
+               //are they positive?
+                  //yes!
+                     //run subtraction (a-b)
+                  //no
+                     //rub subtraction(-a--b) = (-(a-b))
+            //no!
+               //is A negative?
+                  //yes!
+                     //run addition (-a-b) (-16-13 = 29)
+                  //no!
+                     //run addition (a--b) (32--16 = 48)
    return result;
 }
 
