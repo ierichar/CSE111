6fe35552c5d85e005a00a55c1c2615ed0f3d5da8
Jackson Ryan Brazeal <jbrazeal@unix2.lt.ucsc.edu>
2021-10-11 22:20:17 -0700

    Added logic for bigint + and -

--- asg1-dc-bigint/code/bigint.cpp
+++ asg1-dc-bigint/code/bigint.cpp
@@ -23,57 +23,99 @@ bigint::bigint (const string& that) {
 }
 
 bigint bigint::operator+ () const {
-   //is A string larger than B string?
-      //yes!
-         //are the signs the same?
-            //yes!
-               //are they positive?
-                  //yes!
-                     //Run addition (A+B)
-                  //no!
-                     //Run addition flip sign(-A+-B)
    return *this;
 }
 
 bigint bigint::operator- () const {
-   // WORK IN PROGRESS
-   //first step, check size of A and B
-
-   //is A larger than B?
-      //it's gonna  stay the sign of the larger #
-   //is B larger than A?
-      //it's gonna stay the sign of the larger #
-   //else, They are the same size
-
-   //Are they the same sign?
-      //yes?
-         //is the larger string first?
-            //yes?
-               //stays that sign
-            //no?
-               //flip signs
-      //no? 
-         //is the larger string first?
-            //yes?
-               //stays that sign
-            //no?
-               //flip sign
-
-
-
-   //subbing two pos #'s that leads to a positive
-
-   // subbing two pos #'s that lead to a negative
-
-   //subbing two neg #'s that lead to a positive
-
-   //subbing two neg #'s that lead to a negative
    return {uvalue, not is_negative};
 }
 
 bigint bigint::operator+ (const bigint& that) const {
-//check for negatives in each one, subtract 1 respectively
+//uvalue(); that.uvalue();
+bool val1negative = false;
+int val1RealSize = uvalue.size();
+bool val2negative = false;
+int val2RealSize = that.uvalue.size();
 
+//check element of each one
+if(uvalue[0] == "_"){
+   val1negative = true;
+   val1RealSize = val1RealSize - 1;
+}
+
+if(that.uvalue[0] == "_"){
+   val2negative = true;
+   val2RealSize = val2RealSize - 1;
+}
+
+bool isval1Larger = false;
+int temp1 = 0;
+int temp2 = 0;
+
+if (val1RealSize == val2RealSize){
+   if(val1negative){
+      temp1 = uvalue[1];
+   }
+   else{
+      temp1 = uvalue[0];
+   }
+   if(val2negative){
+      temp2 = that.uvalue[1];
+   }
+   else{
+      temp2 = that.uvalue[0];
+   }
+
+   if(temp1 > temp2){
+      isval1Larger = true;
+   }
+
+   //now that you know which value is larger, you can run the code below!
+}
+
+if(isval1Larger){
+   if((val1negative==false)&&(val2negative==false)){
+      ubigint result {uvalue + that.uvalue};
+
+   } 
+   if((val1negative==true)&&(val2negative==true)){
+      ubigint result {uvalue + that.uvalue};
+      result.is_negative();
+   }
+   if((val1negative==true)&&(val2negative==false)){
+       ubigint result {uvalue - that.uvalue};
+       result.is_negative();
+      //run subtraction (-a+b) add negative
+      
+   }
+   else{
+       ubigint result {uvalue - that.uvalue};
+      //run subtraction (a+-b), A-B stay positive
+   }
+}
+   else{ //B is larger now
+      if((val1negative==false)&&(val2negative==false)){
+         ubigint result {that.uvalue + uvalue};
+      //do addition B+A
+   } 
+   if((val1negative==true)&&(val2negative==true)){
+      ubigint result {that.uvalue + uvalue};
+      result.is_negative();
+      //do addition flip sign (-b+-a)
+   }
+   if((val1negative==true)&&(val2negative==false)){
+      ubigint result {that.uvalue - uvalue};
+      result.is_negative();
+      //run subtraction (-b+a) add negative
+   }
+   else{
+      //run subtraction (b+-a), B-A stay positive
+      ubigint result {that.uvalue - uvalue};
+   }
+}
+//both of those subtract 1 from a size to account for the negative taking up 1 space.
+
+//check for negatives in each one, subtract 1 respectively
 
    //are strings same size?
       //create a flag, isBLarger? yes, run first loop! no? run second loop.
@@ -100,11 +142,94 @@ bigint bigint::operator+ (const bigint& that) const {
                
       //no!
 
-   ubigint result {uvalue + that.uvalue};
+   //ubigint result {uvalue + that.uvalue};
    return result;
 }
 
 bigint bigint::operator- (const bigint& that) const {
+   //uvalue(); that.uvalue();
+bool val1negative = false;
+int val1RealSize = uvalue.size();
+bool val2negative = false;
+int val2RealSize = that.uvalue.size();
+
+//check element of each one
+if(uvalue[0] == "_"){
+   val1negative = true;
+   val1RealSize = val1RealSize - 1;
+}
+
+if(that.uvalue[0] == "_"){
+   val2negative = true;
+   val2RealSize = val2RealSize - 1;
+}
+
+bool isval1Larger = false;
+int temp1 = 0;
+int temp2 = 0;
+
+if (val1RealSize == val2RealSize){
+   if(val1negative){
+      temp1 = uvalue[1];
+   }
+   else{
+      temp1 = uvalue[0];
+   }
+   if(val2negative){
+      temp2 = that.uvalue[1];
+   }
+   else{
+      temp2 = that.uvalue[0];
+   }
+
+   if(temp1 > temp2){
+      isval1Larger = true;
+   }
+
+   //now that you know which value is larger, you can run the code below!
+}
+
+if(isval1Larger){
+   if((val1negative==false)&&(val2negative==false)){
+      //do subtraction A-b
+      ubigint result {uvalue - that.uvalue};
+   } 
+   if((val1negative==true)&&(val2negative==true)){
+      //do subtraction a-b slap on a negative
+      ubigint result {uvalue - that.uvalue};
+      result.is_negative();
+   }
+   if((val1negative==true)&&(val2negative==false)){
+      //addition slap on a negative (-a-b)
+      ubigint result {uvalue + that.uvalue};
+      result.is_negative();
+   }
+   else{
+      //run addition leave positive (a--b)
+      ubigint result {uvalue + that.uvalue};
+   }
+}
+   else{ //B is larger now
+      if((val1negative==false)&&(val2negative==false)){
+      //do subtraction (b-a)
+      ubigint result {that.uvalue - uvalue};
+   } 
+   if((val1negative==true)&&(val2negative==true)){
+      //do subtraction (-(b-a)) slap on a negative
+      ubigint result {that.uvalue - uvalue};
+      result.is_negative();
+   }
+   if((val1negative==true)&&(val2negative==false)){
+      //run addition (-b-a) put on a negative
+      ubigint result {that.uvalue + uvalue};
+      result.is_negative();
+   }
+   else{
+      //run addition (b--a) keep positive
+      ubigint result {that.uvalue  uvalue};
+   }
+}
+
    //are strings same size?
       //create a flag, isBLarger? yes, run first loop! no? run second loop.
 
@@ -123,7 +248,7 @@ bigint bigint::operator- (const bigint& that) const {
          //no!
             //is A negative?
                //yes!
-                  //run addition (-a-b) (-16-13 = 29)
+                  //run addition (-a-b) (-16-13 = -29)
                //no!
                   //run addition (a--b) (32--16 = 48)
 
